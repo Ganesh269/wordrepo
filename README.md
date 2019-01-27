0684c15c2badb577d727df0f617bce7995afae7b
unknown <greatganesh269@gmail.com>
2019-01-28 01:04:00 +0530

    added memo type functionality(switch case), added ability to view all files at a time

--- README.md
+++
@@ -1,3 +0,0 @@
-# wordrepo
-
-This program is a repository of all the words that iam going to learn, so that it is easy for me to refer them back.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
+#include<math.h>
 using namespace std;
 int MAX=100;
+int n_data_items=0;
 int add(){
     ofstream file("repo.dat",ios::binary|ios::app);
     char str[4][MAX];
@@ -17,7 +19,7 @@ int add(){
     //file.write(reinterpret_cast<char*>(siz),4*sizeof(int));
     for(int i=0;i<4;i++)
         file.write(reinterpret_cast<char*>(&str[i]),MAX*sizeof(char));
-
+    n_data_items++;
     file.close();
     return 1;
 }
@@ -33,22 +35,42 @@ int read(int n){
     }
     file.close();
 }
-int retrieve(int pos=0){
+int retrieve(int pos=1,bool all=false){   //pos indexed from 1
     ifstream file("repo.dat",ios::binary);
-    file.seekg(0,ios::end);
-    int endpoint=file.tellg();
-    int num=endpoint/400;
+    cout<<"came"<<all;
+    file.seekg(0,ios::end);   //goto the end i.e, 0 from the end
+    int endpoint=file.tellg();  // tell the position we are at present i.e, no.of bytes from the beginning
+    int num=endpoint/400;        //since each object is 400 in size
     if(pos>num){
         cout<<"ERROR 400 : INVALID INPUT"<<endl;
         return 0;
     }
     char s[4][MAX];
-    file.seekg((pos-1)*400,ios::beg);
-    for(int i=0;i<4;i++)
-        file.read(reinterpret_cast<char*>(&s[i]),MAX*sizeof(char));
-    for(int i=0;i<4;i++)
-        cout<<s[i]<<" ";
+    if(all){
+        for(int j=1;j<=num;j++){
+            file.seekg((j-1)*400,ios::beg);
+            for(int i=0;i<4;i++)
+                file.read(reinterpret_cast<char*>(&s[i]),MAX*sizeof(char));
+            for(int i=0;i<4;i++)
+                cout<<s[i]<<" ";
+        }
+    }
+    else{
+        file.seekg((pos-1)*400,ios::beg);
+        for(int i=0;i<4;i++)
+            file.read(reinterpret_cast<char*>(&s[i]),MAX*sizeof(char));
+        for(int i=0;i<4;i++)
+            cout<<s[i]<<" ";
+    }
     return 1;
+}
+int tonumber(char t[]){
+    int n=strlen(t),res=0;
+    int placevalue=pow(10,n-1);
+    for(int i=0; i<n;i++,placevalue/=10){
+        res+=placevalue*(t[i]-48);
+    }
+    return res;
 }
 int retrieve(char str[100]){
     ifstream file("repo.dat",ios::binary);
@@ -73,13 +95,34 @@ int retrieve(char str[100]){
     else
         cout<<"ERROR 404 : NOT FOUND"<<endl;
     return 1;
-
-
 }
+
 int main(){
     int entries=0;
-    char a[4]="ma";
-    retrieve(a);
-
-
+    int choice;
+    char cont,temp[100];
+    do{
+        cout<<"\n1.Add 2.Retrieve:";
+        cin>>choice;
+        switch(choice){
+            case 1: add();
+                    break;
+            case 2: cout<<"\nEnter a string or position(1 indexed):";
+                    cin>>temp;
+                    int tmp;
+                    if(!strcmp(temp,"-all"))
+                        retrieve(1,true);
+                    else if(isdigit(temp[0])){
+                        tmp=tonumber(temp);
+                        retrieve(tmp);
+                    }
+                    else
+                        retrieve(temp);
+                    break;
+            default:cout<<"\nEnter Valid Choice";
+        }
+        cout<<"\nContinue (Y/N):";
+        cin>>cont;
+    }while(cont!='n');
+    return 0;
 }
